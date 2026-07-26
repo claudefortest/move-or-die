846fd5a092737e5094022aec77d339552b696481
claudefortest <claudefortest@gmail.com>
2026-07-26 02:44:33 +0000

    Fix timer label visibility, show 0 on death, safer coin pickup collision removal

--- main.gd
+++ main.gd
@@ -3,23 +3,32 @@ extends Node2D
 @onready var timer = $CountDownTimer
 @onready var label = $CanvasLayer/TimerLabel
 
+var is_dead := false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	label.text = str(ceil(timer.time_left))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	
-	label.text = str(ceil(timer.time_left))
-	
-	if timer.time_left < 3.0:
+	if is_dead:
+		return
+
+	label.text = str(int(ceil(timer.time_left)))
+
+	if timer.time_left <= 3.0:
 		label.modulate = Color.RED
-		
 	else:
 		label.modulate = Color.WHITE
-		
+
 func _on_count_down_timer_timeout() -> void:
+	is_dead = true
+	label.text = "0"
+	label.modulate = Color.RED
+	get_tree().paused = true
+	await get_tree().create_timer(0.6, true, false, true).timeout
+	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 
@@ -28,4 +37,4 @@ func _on_time_reset_body_entered(body):
 		timer.start() # this resets the ten second clock back to full
 		$TimeReset/PickupSound.play()
 		$TimeReset/Sprite2D.hide()
-		$TimeReset/CollisionShape2D.queue_free() 
+		$TimeReset/CollisionShape2D.set_deferred("disabled", true)
