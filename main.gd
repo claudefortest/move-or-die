e79e7bdae5c925d293dfdad0ceff95cc4a267536
subhaintdead <subhgotbeans@gmail.com>
2026-07-26 07:06:42 +0530

    GGG

---
+++ main.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+@onready var timer = $CountDownTimer
+@onready var label = $CanvasLayer/TimerLabel
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	
+	label.text = str(ceil(timer.time_left))
+	
+	if timer.time_left < 3.0:
+		label.modulate = Color.RED
+		
+	else:
+		label.modulate = Color.WHITE
+		
+func _on_countdown_timer_timeout():
+	get_tree().reload_current_scene()
+
+
+func _on_count_down_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_time_reset_body_entered(body):
+	if body.name == "Player":
+		timer.start() # this resets the ten second clock back to full
+		$TimeReset/PickupSound.play()
+		$TimeReset/Sprite2D.hide()
+		$TimeReset/CollisionShape2D.queue_free() 
